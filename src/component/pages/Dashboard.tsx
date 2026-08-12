436cf25f685375793313e882c04cb8931e344e35
Brandon Jablasone <BrandonJablasone@gmail.com>
2026-08-11 21:11:17 -0400

    feat: add placeholder page components for navigation

---
+++ src/component/pages/Dashboard.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+export const DashboardLeftContentArear = () => {
+    return (
+        <div className="w-ful h-full">
+            Dash Board
+        </div>
+    );
+};
+
+export const DashboardRightContentArear = () => {
+    return (
+        <div className="w-ful h-full">
+            Dash Board
+        </div>
+    )
+}
---
+++ src/component/pages/Jobs.tsx
@@ -0,0 +1,21 @@
+// @flow
+
+
+
+
+
+export const JobsLeftContentArear = () => {
+    return (
+        <div className="w-ful h-full">
+
+        </div>
+    );
+};
+
+export  const JobsRightContentArear = () => {
+    return (
+        <div className="w-full h-full">
+
+        </div>
+    )
+}
